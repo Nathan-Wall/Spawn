023c6877c45b6ce73cd551f1c58d7501b365c3fb
Nathan Wall <nwall@appnexus.com>
2012-10-26 01:10:43 -0400

    Updated several things, including: Added extend method to Things, improved inheritance model, and made PROTO object publicly accessible and replaceable.

--- poise.js
+++ poise.js
@@ -8,7 +8,8 @@ var poise = (function() {
 		// Basically, this object provides spawns with Object.prototype properties, but it also
 		// allows all spawned objects' prototype to be modified directly with
 		// Object.getPrototypeOf(spawnedObject).newProp = value;
-		PROTO = { },
+		// We use the syntax below (instead of symply PROTO = { }) so that spawns have the name "spawn" when logged.
+		PROTO = (function spawn() { }).prototype,
 
 		beget = function beget(/* proto, props */) {
 
@@ -19,19 +20,82 @@ var poise = (function() {
 
 		},
 
+		lazyBind = Function.prototype.bind.bind(Function.prototype.call),
+		lazyTie = Function.prototype.bind.bind(Function.prototype.apply),
+		slice = lazyBind(Array.prototype.slice),
+		isPrototypeOf = lazyBind(Object.prototype.isPrototypeOf),
+
+		// Creates a wrapper function with the same length as the original.
+		createWrapper = (function() {
+
+			// Let's memoize wrapper generators to avoid using eval too often.
+			var generators = { },
+
+				numGenerators = 0;
+
+			return function createWrapper(original, f) {
+
+				var length = original.length,
+					args = [ ],
+					generator = generators[length];
+
+				if (!generator) {
+
+					for(var i = 0; i < length; i++)
+						args.push('$' + i);
+
+					generator = eval('(function(f, original) { var wrapper = function ' + original.name + '(' + args.join(',') + ') { return f.apply(this, arguments); }; wrapper.original = original; return wrapper; })');
+
+					// Limit the number of generators which are cached to preserve memory in the unusual case that someone
+					// creates many generators. We don't go to lengths to make the cache drop old, unused values as there
+					// really shouldn't be a need for so many generators in the first place.
+					if (numGenerators < 64) {
+						generators[length] = generator;
+						numGenerators++;
+					}
+
+				}
+
+				return generator(f, original);
+
+			};
+
+		})(),
+
+
+		invert = function invert(f/*, length*/) {
+			var length = arguments[1];
+			return createWrapper(f, function wrapper() {
+				var args;
+				if (length !== undefined) {
+					args = slice(arguments, 0, length);
+					args.length = length;
+				} else {
+					args = slice(arguments);
+				}
+				return f.apply(null, args.reverse());
+			});
+		},
+
+		isA = invert(isPrototypeOf, 2),
+
 		reBase = /\.\s*base\b/,
 
 		propsToDescriptors = function propsToDescriptors(props, proto) {
 
-			var desc = { };
+			var desc = { },
+
+				protoIsObject = Object(proto) === proto;
 
 			Object.getOwnPropertyNames(props).forEach(function(name) {
 
 				var d = Object.getOwnPropertyDescriptor(props, name);
-				//d.enumerable = false;
+				d.enumerable = false;
 
 				if (
-					hasOwn(d, 'value')
+					// Only Things are magicWrapped to allow a special base method.
+					protoIsObject && Thing && isA(proto, Thing)
+					&& hasOwn(d, 'value')
 					&& typeof d.value == 'function'
 					&& typeof proto[name] == 'function'
 					&& reBase.test(d.value.toString())
@@ -79,45 +143,79 @@ var poise = (function() {
 
 		},
 
-		// Creates a wrapper function with the same length as the original.
-		createWrapper = (function() {
+		contextualize = function contextualize(f/*, arg1, arg2, ... */) {
 
-			// Let's memoize wrapper generators to avoid using eval too often.
-			var generators = { },
+			if (typeof f != 'function')
+				throw new TypeError('Function expected: ' + f);
 
-				numGenerators = 0;
+			var doF = lazyTie(f),
+				args = slice(arguments, 1);
 
-			return function createWrapper(original, f) {
+			function contextualizedMethod() {
+				return doF(null, [ this ].concat(args, slice(arguments)));
+			}
 
-				var length = original.length,
-					args = [ ],
-					generator = generators[length];
+			contextualizedMethod.original = f;
+			return contextualizedMethod;
 
-				if (!generator) {
-
-					for(var i = 0; i < length; i++)
-						args.push('$' + i);
-
-					generator = eval('(function(f, original) { var wrapper = function ' + original.name + '(' + args.join(',') + ') { return f.apply(this, arguments); }; wrapper.original = original; return wrapper; })');
-
-					// Limit the number of generators which are cached to preserve memory in the unusual case that someone
-					// creates many generators. We don't go to lengths to make the cache drop old, unused values as there
-					// really shouldn't be a need for so many generators in the first place.
-					if (numGenerators < 64) {
-						generators[length] = generator;
-						numGenerators++;
-					}
-
-				}
-
-				return generator(f, original);
-
-			};
-
-		})(),
+		},
 
 		spawn = function spawn(props) {
-			return beget(PROTO, props);
+			// Use poise.PROTO instead of just PROTO below to allow the PROTO proprety to be reassigned.
+			return beget(poise && poise.PROTO || PROTO, props);
+		},
+
+		getUncommonPropertyNames = (function() {
+			return function getUncommonPropertyNames(from, compareWith) {
+				var namesMap = Object.create(null);
+				return concatUncommonNames(from, compareWith)
+					.filter(function(u) {
+						if (namesMap[u]) return false;
+						namesMap[u] = true;
+						return true;
+					});
+			};
+			function concatUncommonNames(from, compareWith) {
+				if (Object(from) != from
+					|| from === compareWith
+					|| isA(compareWith, from)) return [ ];
+				return Object.getOwnPropertyNames(from).concat(
+					concatUncommonNames(Object.getPrototypeOf(from), compareWith));
+			}
+		})(),
+
+		getPropertyDescriptor = function getPropertyDescriptor(obj, name) {
+			if (Object(obj) !== obj) return undefined;
+			return Object.getOwnPropertyDescriptor(obj, name)
+				|| getPropertyDescriptor(Object.getPrototypeOf(obj), name);
+		},
+
+		extend = function extend(extendWhat/*, obj2, obj3, ... */) {
+
+			var extendWith;
+
+			if(Object(extendWhat) != extendWhat)
+				throw new TypeError('first argument must be an object: ' + extendWhat);
+
+			for(var i = 1; i < arguments.length; i++) {
+				extendWith = Object(arguments[i]);
+				getUncommonPropertyNames(extendWith, extendWhat).forEach(function(name) {
+					var whatDesc = getPropertyDescriptor(extendWhat, name),
+						withDesc = getPropertyDescriptor(extendWith, name);
+					if(!whatDesc || whatDesc.configurable) {
+						// If extendWhat does not already have the property, or if extendWhat
+						// has the property and it's configurable, add it as is.
+						Object.defineProperty(extendWhat, name, withDesc);
+					} else if(whatDesc.writable && withDesc.value) {
+						// If the property is writable and the withDesc has a value, write the value.
+						// TODO: rethink this?
+						extendWhat[name] = withDesc.value;
+					}
+				});
+			}
+
+			return extendWhat;
+
 		},
 
 		Thing = spawn({
@@ -126,17 +224,19 @@ var poise = (function() {
 				throw new Error('base method called outside of magic method.');
 			},
 
-			beget: function beget(props) {
-				return poise.beget(this, props);
-			}
+			beget: contextualize(beget),
+			isA: contextualize(isA),
+			extend: contextualize(extend)
 
 		}),
 
 		poise = spawn({
 
-			hasOwn: hasOwn,
+			PROTO: PROTO,
+
 			spawn: spawn,
 			beget: beget,
+			isA: isA,
 
 			Thing: Thing
 
